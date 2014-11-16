cce84485054ff4418dff204460597543cdf4e0c8
Michael Hancock <mikehan99@hotmail.com>
2014-11-16 09:59:21 +0000

    adding integration test

---
+++ CypherTwo/CypherTwo.Tests/IntegrationTests.cs
@@ -0,0 +1,33 @@
+﻿namespace CypherTwo.Tests
+{
+    using System.Net.Http;
+
+    using CypherTwo.Core;
+
+    using NUnit.Framework;
+
+    [TestFixture]
+    public class IntegrationTests
+    {
+        private INeoClient neoClient;
+
+        private ISendRestCommandsToNeo neoApi;
+
+        private IJsonHttpClientWrapper httpClientWrapper;
+
+        [SetUp]
+        public void SetupBeforeEachTest()
+        {
+            this.httpClientWrapper = new JsonHttpClientWrapper(new HttpClient());
+            this.neoApi = new NeoRestApiClient(this.httpClientWrapper, "http://localhost:7474/");
+            this.neoClient = new NeoClient(this.neoApi);
+        }
+
+        [Test]
+        public void GetPersonNode()
+        {
+            this.neoClient.Connect();
+
+        }
+    }
+}
